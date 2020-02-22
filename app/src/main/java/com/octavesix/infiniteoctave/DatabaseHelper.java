79b4f1ff39eefd26e247019d5bbfc72e4e6bd53d
siarra <sgrem13@lsu.edu>
2020-02-21 20:01:42 -0600

    add account database

---
+++ app/src/main/java/com/octavesix/infiniteoctave/DatabaseHelper.java
@@ -0,0 +1,49 @@
+package com.octavesix.infiniteoctave;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(Context context) {
+        super(context,"Login.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("Create table user(username text primary key, password text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists user");
+    }
+    public boolean insert(String username, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("username", username);
+        contentValues.put("password", password);
+        long ins = db.insert("user", null, contentValues);
+        if(ins == -1)
+            return false;
+        else
+            return true;
+    }
+    public Boolean checkUsername(String username){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("Select * from user where username=?", new String[] {username});
+        if(cursor.getCount() > 0)
+            return true;
+        else
+            return false;
+    }
+    public Boolean usernamePassword(String username, String password){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("Select * from user where username=? and password=?",new String[]{username, password});
+        if(cursor.getCount() > 0)
+            return true;
+        else
+            return false;
+    }
+}
